09d0e3007d22333f48c3a4864cd352e5b5252d32
iwanhae <iwanhae@gmail.com>
2023-08-24 14:33:28 +0900

    fix(searcher): open sqlite in read only mode
    this will reduce memory usage in cloud run

--- searcher/src/database/mod.rs
+++ searcher/src/database/mod.rs
@@ -2,9 +2,11 @@ use std::env;
 
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::OpenFlags;
 
 pub fn new_connection_pool() -> Pool<SqliteConnectionManager> {
     let database_url = env::var("DATABASE_URL").unwrap_or_else(|_| String::from("./kuberian.db"));
-    let manager = SqliteConnectionManager::file(database_url);
+    let manager =
+        SqliteConnectionManager::file(database_url).with_flags(OpenFlags::SQLITE_OPEN_READ_ONLY);
     Pool::new(manager).expect("fail to generate db connection pool")
 }
