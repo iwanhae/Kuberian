a8cf3b652f3abd142f78fb277db563954070c932
iwanhae <iwanhae@gmail.com>
2023-08-25 15:19:44 +0000

    feat(web): defined a dev container

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,24 +1,24 @@
-export const runtime = 'edge';
+export const runtime = 'edge'
 
-async function getData(): Promise<{
-  total: number;
-  analyzed: number;
-  samples: string[];
+async function getData (): Promise<{
+  total: number
+  analyzed: number
+  samples: string[]
 }> {
-  const res = await fetch("https://kuberian-thoe7ww5ya-an.a.run.app/", {next: {revalidate: 5}});
+  const res = await fetch('https://kuberian-thoe7ww5ya-an.a.run.app/', { next: { revalidate: 5 } })
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
+    throw new Error('Failed to fetch data')
   }
 
-  return res.json();
+  return await res.json()
 }
 
-export default async function Page() {
-  const data = await getData();
+export default async function Page (): Promise<JSX.Element> {
+  const data = await getData()
 
   return (
     <main>
@@ -26,9 +26,9 @@ export default async function Page() {
       <h5>Analyzed: {data.analyzed}</h5>
       <ol>
         {data.samples.map((v, i) => {
-          return <li key={i}>{v}</li>;
+          return <li key={i}>{v}</li>
         })}
       </ol>
     </main>
-  );
+  )
 }
