dd15c3e464f084283bfac89186a2ff1db51f05cd
iwanhae <iwanhae@gmail.com>
2023-09-30 15:09:55 +0000

    feat(web): change landing description

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,78 +1,83 @@
 import Card from "@/components/card";
-import { SERVER_URL } from "@/config";
 import Header from "./header";
 
 export const runtime = "edge";
 
-async function getData(): Promise<{
-  total: number;
-  analyzed: number;
-  samples: string[];
-}> {
-  const res = await fetch(SERVER_URL(), {
-    next: { revalidate: 5 },
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return await res.json();
-}
-
 export default async function Page(): Promise<JSX.Element> {
-  const data = await getData();
+  const samples = [
+    "checks if the system supports IPVS.",
+    "authenticates the user request with certification.",
+    "retrieves actual usage of CPU from container.",
+    "checks available memory for each NUMA nodes.",
+    "handles OOM events from the system.",
+    "authenticates HTTP requests with x509 certificates.",
+    "checks if the node has enough memory to deploy pod.",
+    "generates service account token for pod.",
+    "pulls an container image with image pull secret.",
+    "registers a GPU device to kubelet",
+    "creates iptable rules for network policy",
+  ];
 
   return (
     <>
       <Header />
       <main className="flex flex-col">
         <div className="max-w-3xl w-full m-auto py-8 gap-4">
-          <Card className="flex flex-col gap-1">
+          <Card className="flex flex-col gap-2">
             <p className="text-3xl font-semibold">{`👋 Hi! I'm Kuberian!`} </p>
-            <p className="text-xl">{`If you are`}</p>
-            <ol className="list-disc list-outside pl-5 text-xl">
-              <li>
-                Navigating the complex landscape of Kubernetes and encountering
-                undocumented features? 😵‍💫
-              </li>
-              <li>
-                Seeking clarity on intricate functionalities buried within the
-                source code? 🤯
-              </li>
-            </ol>
-            <p className="text-xl">{`I'm here to assist you! Here's how to harness the power of Kuberian: 🚀`}</p>
+            <hr className="my-4 h-0.5 border-t-0 bg-neutral-100 opacity-100" />
             <p className="text-xl">
-              {`Simply describe your query in natural language, like `}
-              <b>{`"find functions that`}</b>
+              {`Try `}
+              <b>{`find functions that`}</b>
             </p>
-            <div className="my-4 shadow-md flex flex-col text-xl font-medium">
-              {data.samples
+            <ul className="text-xl list-disc list-inside">
+              {samples
+                .map((value) => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value)
+                .slice(0, 4)
                 .sort((a, b) => (a.length < b.length ? -1 : 1))
-                .slice(0, 5)
                 .map((v, i) => {
                   return (
-                    <a
-                      key={i}
-                      className="p-2 border first:rounded-t-lg last:rounded-b-lg odd:bg-slate-100 hover:bg-sky-200 cursor-pointer transition-all text-ellipsis whitespace-nowrap overflow-hidden"
-                      href={`/q/${v}`}
-                    >
-                      {v}
-                    </a>
+                    <li key={i}>
+                      <a
+                        className="text-[#006bb8] hover:underline"
+                        href={`/q/${v}`}
+                      >
+                        {v}
+                      </a>
+                    </li>
                   );
                 })}
-            </div>
+            </ul>
             <p className="text-xl">
-              <b>{`from Kubernetes." `}</b>
-              {`Our intelligent search will sift through the Kubernetes source
-              code and pinpoint the exact functions you need.`}
+              <b>{`from Kubernetes `}</b> with Kuberian.
+            </p>
+            <hr className="w-1/2 m-auto h-0.5 border-t-0 bg-neutral-100 opacity-100" />
+            <p className="text-xl">
+              <b>Kuberian</b> is a Kubernetes code search service designed for
+              DevOps Engineers or SREs seeking detailed information about
+              Kubernetes. Just describe the functions you want to explore in the
+              search box using natural language.
             </p>
             <p className="text-xl">
-              Say goodbye to guesswork and hours of sifting through code!
-              Welcome to a hassle-free journey of understanding Kubernetes
-              intricacies. 🌟
+              With the power of the{" "}
+              <a
+                href="https://huggingface.co/TheBloke/Llama-2-7B-GGML"
+                className="hover:underline text-blue-700"
+              >
+                Llama 2 7B
+              </a>{" "}
+              and{" "}
+              <a
+                href="https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2"
+                className="hover:underline text-blue-700"
+              >
+                all-MiniLM-L6-v2
+              </a>
+              , you will find whatever you want to know much faster. 🚀🚀
             </p>
-            <p></p>
+            <p className="text-xl">{"🤞 🍀 🌊 🔎 ⎈"}</p>
           </Card>
         </div>
       </main>
